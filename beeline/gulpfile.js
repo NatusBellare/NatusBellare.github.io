c6add15f94faf6ca496c9d05a9bbdc385d0f6c97
Igor Suvorov <gawr230@mail.ru>
2018-12-09 17:22:45 +0500

    1.2.3

--- beeline/gulpfile.js
+++ beeline/gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.js' // Always at the end
+		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Минимизировать весь js (на выбор)
